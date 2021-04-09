670a900289f028def7eeb3a28bf0e7ca2952cf10
joseSoto <jose.soto@zestspain.com>
2021-04-09 23:00:56 +0200

    heroku test final...?

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname));
-console.log(__dirname);
+app.use(express.static(__dirname+'/dist/mangoCandidaturaJose'));
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/src/app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/mangoCandidaturaJose/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
